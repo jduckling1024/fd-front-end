554849fe170f77c2c5e0e93c15d33b2fec6aedb2
jduckling1024 <jduck1024@naver.com>
2021-12-02 21:12:02 +0900

    Chore : 프로젝트 환경 설정
    - eslint 및 prettier 설치
    - Redux 설치

---
+++ app/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "no-undef": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+
+  // Parsing error: The keyword 'import' is reserved eslint 해결
+  parser: "babel-eslint",
+};
