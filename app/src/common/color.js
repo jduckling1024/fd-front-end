38d7456d5d5641ae89680b5889eae58560159773
jduckling1024 <jduck1024@naver.com>
2021-12-08 22:48:28 +0900

    Feat : 테마 설정

---
+++ app/src/common/color.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const concept = createTheme({
+  palette: {
+    primary: {
+      main: "#51DACF",
+      contrastText: "#FFF",
+    },
+    secondary: {
+      main: "#41AAA8",
+    },
+  },
+});
+
+export default concept;
